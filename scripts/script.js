0be50c7e76fedd2b5cdc82a8f5cdcc438038ab69
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2020-02-25 12:16:28 +0100

    Video Show off

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+ myFunction = () => {
+    let x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  };
